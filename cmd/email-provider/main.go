131725dc0c4550e6edbc02961aa7e2a493cbf0a0
ThomasCardin <tomcardin@outlook.com>
2022-02-13 15:34:02 -0500

    grpc interface implementation for email

--- cmd/email-provider/main.go
+++ cmd/email-provider/main.go
@@ -1,1 +1,39 @@
 package main
+
+import (
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+
+	context "context"
+
+	email_provider "github.com/clubcedille/calidum-rotae-backend/pkg/proto-gen/email-provider"
+)
+
+type Server struct {
+	email_provider.EmailProviderServer
+}
+
+func (server *Server) SendMessage(ctx context.Context, message *email_provider.SendEmailRequest) (*email_provider.SendEmailResponse, error) {
+	log.Printf("Received message content from client: %s", message.Sender)
+	// Sends a message to a Discord webhook here
+	return &email_provider.SendEmailResponse{}, nil
+}
+
+const PORT string = "PORT"
+
+func main() {
+	lt, err := net.Listen("tcp", PORT)
+	if err != nil {
+		log.Fatalf("Error starting tcp listener on %s : %v", PORT, err)
+	}
+
+	server := Server{}
+	grpcServer := grpc.NewServer()
+	email_provider.RegisterEmailProviderServer(grpcServer, &server)
+
+	if err := grpcServer.Serve(lt); err != nil {
+		log.Fatalf("Failed to serve gRPC server over port %s : %v", PORT, err)
+	}
+}
