76053a834e6106ede56be0be5516dd65c72338a5
Charles Guertin <charlesguertin@live.ca>
2022-07-20 18:59:24 -0400

    Update flags of calidum rotae service to add db

--- cmd/calidum-rotae-service/config/config.go
+++ cmd/calidum-rotae-service/config/config.go
@@ -14,6 +14,16 @@ const (
 	// Email provider settings
 	FlagEmailProviderHostname = "email_provider_hostname"
 	FlagEmailProviderPort     = "email_provider_port"
+
+	// Database settings
+	defaultDBPort uint32 = 5432
+	dbPrefix             = "db-"
+	FlagDbHost           = dbPrefix + "host"
+	FlagDbPort           = dbPrefix + "port"
+	FlagDbName           = dbPrefix + "name"
+	FlagDbUser           = dbPrefix + "user"
+	FlagDbSchema         = dbPrefix + "schema"
+	FlagDbSslMode        = dbPrefix + "ssl"
 )
 
 func SetFlags(cmd *cobra.Command) {
@@ -25,6 +35,13 @@ func SetFlags(cmd *cobra.Command) {
 
 	cmd.Flags().String(FlagEmailProviderHostname, "email_provider", "The email provider microservice's hostname to connect to")
 	cmd.Flags().Uint32(FlagEmailProviderPort, 0, "The email provider microservice's port to connect to")
+
+	cmd.Flags().String(FlagDbHost, "postgres", "the database host to connect to")
+	cmd.Flags().Uint32(FlagDbPort, defaultDBPort, "the database host to connect to")
+	cmd.Flags().String(FlagDbName, "calidum_rotae", "the database name to connect to")
+	cmd.Flags().String(FlagDbUser, "postgres", "the database user to use to connect to the database")
+	cmd.Flags().String(FlagDbSslMode, "require", "[verify-ca, verify-full, require, disable]")
+	cmd.Flags().String(FlagDbSchema, "calidum_rotae", "the database schema to use")
 
 	cmd.MarkFlagRequired(FlagPort)
 }
