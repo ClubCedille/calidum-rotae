9c803a4bf5fdc2caa0b3c0bf90d2527594965888
ThomasCardin <tomcardin@outlook.com>
2022-09-03 15:37:18 -0400

    necessary struct to send a embedded discord message

---
+++ cmd/discord-provider/server/template.go
@@ -0,0 +1,19 @@
+package server
+
+// Necessary structures for the creation of a embedded discord message
+type Message struct {
+	Username  string     `json:"username,omitempty"`
+	Content   string     `json:"content,omitempty"`
+	Embeddeds []Embedded `json:"embeds,omitempty"`
+}
+
+type Embedded struct {
+	Title       string `json:"title,omitempty"`
+	Description string `json:"description,omitempty"`
+	Color       string `json:"color,omitempty"`
+	Footer      Footer `json:"footer,omitempty"`
+}
+
+type Footer struct {
+	Text string `json:"text,omitempty"`
+}
