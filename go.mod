1deb384042d25445d1ab65d1945f476e25ea6f21
Thomas Cardin <49320132+ThomasCardin@users.noreply.github.com>
2023-11-22 16:09:56 -0500

    added instrumentation foreach query

--- go.mod
+++ go.mod
@@ -22,9 +22,17 @@ require (
 	github.com/bytedance/sonic v1.9.1 // indirect
 	github.com/chenzhuoyu/base64x v0.0.0-20221115062448-fe3a3abad311 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.2 // indirect
+	github.com/go-logr/logr v1.3.0 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.4 // indirect
 	github.com/sendgrid/rest v2.6.9+incompatible // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
+	go.opentelemetry.io/otel v1.21.0 // indirect
+	go.opentelemetry.io/otel/exporters/stdout/stdoutmetric v0.44.0 // indirect
+	go.opentelemetry.io/otel/metric v1.21.0 // indirect
+	go.opentelemetry.io/otel/sdk v1.21.0 // indirect
+	go.opentelemetry.io/otel/sdk/metric v1.21.0 // indirect
+	go.opentelemetry.io/otel/trace v1.21.0 // indirect
 	golang.org/x/arch v0.3.0 // indirect
 	golang.org/x/lint v0.0.0-20210508222113-6edffad5e616 // indirect
 	golang.org/x/tools v0.6.0 // indirect
@@ -64,7 +72,7 @@ require (
 	github.com/volatiletech/randomize v0.0.1 // indirect
 	golang.org/x/crypto v0.9.0 // indirect
 	golang.org/x/net v0.10.0 // indirect
-	golang.org/x/sys v0.8.0 // indirect
+	golang.org/x/sys v0.14.0 // indirect
 	golang.org/x/text v0.9.0 // indirect
 	golang.org/x/xerrors v0.0.0-20220517211312-f3a8303e98df // indirect
 	google.golang.org/genproto v0.0.0-20230110181048-76db0878b65f // indirect
--- pkg/calidum/calidum.go
+++ pkg/calidum/calidum.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"fmt"
 
+	instrumentation "github.com/clubcedille/calidum-rotae-backend/cmd/otel-instrumentation"
 	discord_provider "github.com/clubcedille/calidum-rotae-backend/pkg/proto-gen/discord-provider"
 	email_provider "github.com/clubcedille/calidum-rotae-backend/pkg/proto-gen/email-provider"
 )
@@ -12,10 +13,10 @@ import (
 type CalidumService struct {
 	discordProviderService discord_provider.DiscordProviderClient
 	emailProviderService   email_provider.EmailProviderClient
+	tracers                instrumentation.Tracer
 }
 
 type CalidumClient interface {
-	// TODO: Define methods to speak with database
 	SendDiscordRpcRequest(ctx context.Context, body []byte) (err error)
 	SendEmailRpcRequest(ctx context.Context, body []byte) (err error)
 }
@@ -25,19 +26,26 @@ var _ CalidumClient = &CalidumService{}
 type Dependencies struct {
 	DiscordProviderService discord_provider.DiscordProviderClient
 	EmailProviderService   email_provider.EmailProviderClient
+	Tracer                 instrumentation.Tracer
 }
 
 func NewCalidumService(deps Dependencies) *CalidumService {
 	return &CalidumService{
 		discordProviderService: deps.DiscordProviderService,
 		emailProviderService:   deps.EmailProviderService,
+		tracers:                deps.Tracer,
 	}
 }
 
 func (c *CalidumService) SendDiscordRpcRequest(ctx context.Context, body []byte) (err error) {
+	_, span := c.tracers.StartSpanAndInitTracers(ctx, instrumentation.DISCORD_PROVIDER_SERVICE)
+	defer span.End()
+
 	var data *discord_provider.SendMessageRequest
 	if err = json.Unmarshal(body, &data); err != nil {
-		return fmt.Errorf("error binding JSON data to gRPC discord object: %s", err.Error())
+		errorMessage := fmt.Sprintf("error binding JSON data to gRPC discord object: %s", err.Error())
+		span = c.tracers.AddErrorEvent(ctx, span, errorMessage)
+		return fmt.Errorf(errorMessage)
 	}
 
 	resp, err := c.discordProviderService.SendMessage(ctx, &discord_provider.SendMessageRequest{
@@ -46,15 +54,23 @@ func (c *CalidumService) SendDiscordRpcRequest(ctx context.Context, body []byte)
 		RequestDetails: data.RequestDetails,
 	})
 	if err != nil {
-		return fmt.Errorf("error sending rpc request to discord provider: %s Response: %s", err.Error(), resp)
+		errorMessage := fmt.Sprintf("error sending rpc request to discord provider: %s Response: %s", err.Error(), resp)
+		span = c.tracers.AddErrorEvent(ctx, span, errorMessage)
+		return fmt.Errorf(errorMessage)
 	}
 
+	c.tracers.EndSpan(span, "discord message sent")
 	return nil
 }
 
 func (c *CalidumService) SendEmailRpcRequest(ctx context.Context, body []byte) (err error) {
+	_, span := c.tracers.StartSpanAndInitTracers(ctx, instrumentation.EMAIL_PROVIDER_SERVICE)
+	defer span.End()
+
 	var data *email_provider.SendEmailRequest
 	if err = json.Unmarshal(body, &data); err != nil {
+		errorMessage := fmt.Sprintf("error binding JSON data to gRPC email object: %s", err.Error())
+		span = c.tracers.AddErrorEvent(ctx, span, errorMessage)
 		return fmt.Errorf("error binding JSON data to gRPC email object: %s", err.Error())
 	}
 
@@ -64,8 +80,11 @@ func (c *CalidumService) SendEmailRpcRequest(ctx context.Context, body []byte) (
 		RequestDetails: data.RequestDetails,
 	})
 	if err != nil {
-		return fmt.Errorf("error sending rpc request to email provider: %s Response: %s", err.Error(), resp)
+		errorMessage := fmt.Sprintf("error sending rpc request to discord provider: %s Response: %s", err.Error(), resp)
+		span = c.tracers.AddErrorEvent(ctx, span, errorMessage)
+		return fmt.Errorf(errorMessage)
 	}
 
+	c.tracers.EndSpan(span, "email sent")
 	return nil
 }
